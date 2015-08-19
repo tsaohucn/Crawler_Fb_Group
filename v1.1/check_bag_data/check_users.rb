7eebefc5a7b8ec1cbf8c90670d98248d40df5ef2
kaogaau <kaogaau@gmail.com>
2015-08-19 17:42:45 +0800

    ...

--- v1.1/check_bag_data/check_users.rb
+++ v1.1/check_bag_data/check_users.rb
@@ -1,3 +1,13 @@
 require 'mongo'
 Mongo::Logger.logger.level = ::Logger::FATAL
-client = Mongo::Client.new([ '192.168.26.180:27017' ],:database =>'fb_group',:user =>'admin',:password =>'12345')
+client = Mongo::Client.new([ '192.168.26.180:27017' ],:database =>'fb_rawdata',:user =>'admin',:password =>'12345')
+user = Hash.new(0)
+puts "checking users..."
+client[:users].find().each do |doc|
+	user[doc['app_scoped_user_id']] = doc['name'] if doc['name'].size > 1
+end
+#user.each do |k,v|
+#	puts "#{k}\t#{v}"
+#end
+puts "共#{user.size}個可疑使用者重複ID"
+puts "complicate"
